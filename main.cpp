cb574b02d92650bdd1f7e1ae4244b4d8412047c7
Haintz Lilla <Haintz Lilla@DESKTOP-90JNMSS>
2023-09-21 13:16:47 +0200

    hibajavítás

--- main.cpp
+++ main.cpp
@@ -4,30 +4,24 @@ constexpr int N_ELEMENTS = 100;
 
 int main()
 {
-    //1.hiba: hibás név
-    int *b = new int[NELEMENTS];
-    //2.hiba: hiányzó pontosvessző és idézőjel
-    std::cout << '1-100 ertekek duplazasa'
-    //3.hiba: hiányos for ciklus
-    for (int i = 0;)
+    int *b = new int[N_ELEMENTS]; //jó név beírása
+    std::cout << "1-100 ertekek duplazasa"; //idézőjel+pontosvessző
+    for (int i = 0;i<N_ELEMENTS; i++) //for ciklus feltétel+növelés
     {
         b[i] = i * 2;
     }
-    //4.hiba: feltétel nélküli for ciklus
-    for (int i = 0; i; i++)
+    for (int i = 0; i<N_ELEMENTS; i++) //feltétel pótlása
     {
-        //5.hiba: hiányzó pontosveszző és értékiírás
-        std::cout << "Ertek:"
+        std::cout << "Ertek:"<< b[i] << std::endl; //pontosveszző és értékiírás
     }    
     std::cout << "Atlag szamitasa: " << std::endl;
-    //6.hiba: nincs értékadás
-    int atlag;
-    //7.hiba: sima vessző pontosvessző helyett
-    for (int i = 0; i < N_ELEMENTS, i++)
+    int atlag = 0;  //értékadás
+    for (int i = 0; i < N_ELEMENTS; i++)     //pontosvessző
     {
-        atlag += b[i]
+        atlag += b[i]; //pontosvessző
     }
     atlag /= N_ELEMENTS;
     std::cout << "Atlag: " << atlag << std::endl;
+    delete[] b; //memóriafelszabadítás
     return 0;
 }
